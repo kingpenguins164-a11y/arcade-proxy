4947fb14787800dd5b60220f4709ed81f69e17b7
kingpenguins164-a11y <kingpenguins164@gmail.com>
2026-05-27 17:35:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 const cors_proxy = require('cors-anywhere');
 
-const proxyServer = cors_proxy.createServer({
-    originWhitelist: [], // Allows all origins
-    requireHeader: [],
-    removeHeaders: ['cookie', 'cookie2', 'x-frame-options', 'content-security-policy']
-});
+const host = '0.0.0.0';
+const port = process.env.PORT || 8080;
 
-module.exports = (req, res) => {
-    // Reroute the serverless request through the proxy engine
-    req.url = req.url.replace(/^\/api/, ''); 
-    proxyServer.emit('request', req, res);
-};
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: [],
+    removeHeaders: [
+        'cookie',
+        'cookie2',
+        'x-request-start',
+        'x-request-id',
+        'via',
+        'connect-time',
+        'total-route-time'
+    ],
+    // This is the magic sauce that intercepts the website response 
+    // and deletes the iframe blocking headers before sending it to you
+    handleInitialRequest: (req, res, location) => {
+        res.oldWriteHead = res.writeHead;
+        res.writeHead = function(statusCode, headers) {
+            if (headers) {
+                // Delete headers that stop the site from loading in an iframe
+                delete headers['x-frame-options'];
+                delete headers['content-security-policy'];
+                delete headers['content-security-policy-report-only'];
+                
+                // Allow cross-origin requests
+                headers['access-control-allow-origin'] = '*';
+            }
+            res.oldWriteHead(statusCode, headers);
+        };
+        return false; 
+    }
+}).listen(port, host, () => {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
